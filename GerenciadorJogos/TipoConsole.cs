c8d70124bc56a310ade048bfc71e88b57ad9c98b
Gustavo <gustavohs2004@gmail.com>
2021-07-25 14:07:02 -0300

    Enum console e Enum genero criados

---
+++ GerenciadorJogos/TipoConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorJogos
+{
+    enum TipoConsole
+    {
+        Play_Station_5,
+        Switch,
+        PC,
+        X_Box_Series_X,
+        Outros
+    };
+}
