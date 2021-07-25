3f6c07441935e8bce85968996f5268001729b1b0
Gustavo <gustavohs2004@gmail.com>
2021-07-25 17:03:17 -0300

    FormPrincipal pronto, organizacao de pastas

---
+++ GerenciadorJogos/Forms/FormPrincipal.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace GerenciadorJogos
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.cboOpcoes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInstrucoes = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(255, 113);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // cboOpcoes
+            // 
+            this.cboOpcoes.FormattingEnabled = true;
+            this.cboOpcoes.Items.AddRange(new object[] {
+            "Cadastrar",
+            "Excluir",
+            "Alterar",
+            "Localizar",
+            "Listar jogos"});
+            this.cboOpcoes.Location = new System.Drawing.Point(209, 71);
+            this.cboOpcoes.Name = "cboOpcoes";
+            this.cboOpcoes.Size = new System.Drawing.Size(121, 23);
+            this.cboOpcoes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "O que você deseja fazer?";
+            // 
+            // btnInstrucoes
+            // 
+            this.btnInstrucoes.Location = new System.Drawing.Point(163, 113);
+            this.btnInstrucoes.Name = "btnInstrucoes";
+            this.btnInstrucoes.Size = new System.Drawing.Size(75, 23);
+            this.btnInstrucoes.TabIndex = 3;
+            this.btnInstrucoes.Text = "Instruções";
+            this.btnInstrucoes.UseVisualStyleBackColor = true;
+            this.btnInstrucoes.Click += new System.EventHandler(this.btnInstrucoes_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(67, 113);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(420, 225);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnInstrucoes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboOpcoes);
+            this.Controls.Add(this.btnExecutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerenciador de Jogos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ComboBox cboOpcoes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInstrucoes;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ GerenciadorJogos/Forms/FormPrincipal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GerenciadorJogos
+{
+    public partial class FormPrincipal : Form
+    {
+        //Código para criar tabela SQL
+        //  Create Table dbo.Jogos
+        //(
+        // idJogo int not null IDENTITY(1,1),
+        // codigo int not null,
+        // nome varchar(100),
+        // descricao varchar(max),
+        // console int not null,
+        // genero int not null
+        //)
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Visible = false; //Oculta o formulário
+            MessageBox.Show("Obrigado por usar nosso programa :D", "Volte sempre!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+
+        private void btnInstrucoes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("▶ Seja bem vindo ao gerenciador de jogos. ◀" +
+                "\n●O que você pode fazer: " +
+                "\n•Cadastrar um jogo" +
+                "\n•Excluir um jogo" +
+                "\n•Alterar um jogo" +
+                "\n•Localizar um jogo por nome" +
+                "\n•Listar todos os jogos" +
+                "\n•Listar todos os jogos por gênero" +
+                "\n•Listar todos os jogos por console", "Instruções", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            string escolha = cboOpcoes.Text;
+            if (escolha == "Cadastrar")
+            {
+
+            }
+            else if (escolha == "Excluir")
+            {
+
+            }
+            else if (escolha == "Alterar")
+            {
+
+            }
+            else if (escolha == "Localizar")
+            {
+
+            }
+            else if (escolha == "Listar jogos")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("O item escolhido é inválido, tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                cboOpcoes.Text = "";
+            }
+        }
+    }
+}
