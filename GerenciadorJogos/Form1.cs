b3dee55808173f31b73ca26d4c5aa2073ad1e525
Gustavo <gustavohs2004@gmail.com>
2021-07-25 14:04:20 -0300

    Criando tabela SQL

--- GerenciadorJogos/Form1.cs
+++ GerenciadorJogos/Form1.cs
@@ -6,12 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace GerenciadorJogos
 {
     public partial class Form1 : Form
     {
+        //Código para criar tabela SQL
+        //  Create Table dbo.Jogos
+        //(
+        // idJogo int not null IDENTITY(1,1),
+        // codigo int not null,
+        // nome varchar(100),
+        // descricao varchar(max),
+        // console int not null,
+        // genero int not null
+        //)
+
         public Form1()
         {
             InitializeComponent();
